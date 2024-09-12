1365f5800cd4839bada5cab317e90e5751c1cd07
Matheus Sombra da Silva <matheussombra@rochamarinho.adv.br>
2024-09-12 13:15:02 -0300

    Aplicado spinner no botão de exportar planilha logo após ser clicado

--- resources/views/pages/drc/drc_list.blade.php
+++ resources/views/pages/drc/drc_list.blade.php
@@ -2,7 +2,14 @@
 
     <div class="d-flex justify-content-end mb-4">
         <a href="{{ route('drc.create') }}" class="btn btn-sm btn-primary shadow-sm mx-2">Inserir Novo Acordo</a>
-        <a href="{{ route('drc.export') }}" class="btn btn-sm btn-success shadow-sm">Exportar Planilha <i class="bi bi-file-earmark-excel-fill"></i></a>
+        <form method="get" action="{{ route('drc.export') }}" id="exportBtn">
+            <button type="submit" class="btn btn-sm btn-success shadow-sm">
+                <span id="exportBtnSpinner" class="spinner-border spinner-border-sm d-none"></span>
+                <span id="exportBtnText">
+                    Exportar Planilha <i class="bi bi-file-earmark-excel-fill"></i>
+                </span>
+            </button>
+        </form>
     </div>
 
     {{-- Tabela --}}
@@ -24,7 +31,7 @@
                 <tr>
                     {{-- <td hidden class="text-start">{{ $acordo->id }}</td> --}}
                     <td class="text-start">{{ $acordo->localizador_npj }}</td>
-                    <td>{{ $acordo->tipo_recuperacao }}</td>
+                    <td>{{ $acordo->tipoRecuperacaoAux->nome }}</td>
                     <td class="txt-wrap">{{ $acordo->adverso_principal }}</td>
                     <td class="text-center">{{ $acordo->mci }}</td>
                     <td class="text-center">0</td>
@@ -39,5 +46,29 @@
     {{-- Plugins --}}
     @include('includes.datatable')
     @include('includes.toasts')
+
+    {{-- Script --}}
+    @push('js')
+        <script>
+            $(document).ready(function() {
+                // --- Regras no botão de exportar planilha ---
+                $('#exportBtn').submit(function(e) {
+                    // e.preventDefault()
+                    let btn = $(this).find('button')
+                    let btnText = $('#exportBtnText')
+                    let spinner = $('#exportBtnSpinner')
+
+                    btn.prop('disabled', true) // aplica disabled no botão
+                    btnText.text('Exportando...') // altera o texto do botão
+                    spinner.removeClass('d-none') // mostra um spinner no botão
+
+                    // recarrega a página logo após a execução do evento
+                    setTimeout(() => {
+                        location.reload();
+                    }, 500)
+                })
+            })
+        </script>
+    @endpush
 
 </x-layout>
